43effdc91b043eede7c0570a22b1af32a1d116b9
asus <asus@LAPTOP-BD2P4KVT>
2020-10-15 20:13:00 +0700

    new commit

---
+++ src/pboif2/pkg10119060/latihan13/relational/PBOIF210119060Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119060.latihan13.relational;
+
+/**
+ *
+ @author Megianto Adi saputra
+ * Nama         : Megianto Adi Saputra
+ * Kelas        : IF-2
+ * NIM          : 10119060
+ * Deskripsi    : Relational
+ */
+public class PBOIF210119060Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.err.println("a == b = " + (bil1 == bil2));
+        System.err.println("a != b = " + (bil1 != bil2));
+        System.err.println("a > b = " + (bil1 > bil2));
+        System.err.println("a < b = " + (bil1 < bil2));
+        System.err.println("b >= a = " + (bil2 >= bil1));
+        System.err.println("a <= a =" + (bil2 <= bil1));
+    }
+    
+}
